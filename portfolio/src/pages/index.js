205d2b212f013a6a648e2b7370dfa72df9f53420
Gabriel Costa Ferreira <72426601+backcost@users.noreply.github.com>
2023-05-10 20:27:23 +0000

    adding Montserrat font

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -1,16 +1,14 @@
-import Image from 'next/image'
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Gabriel | FullStack Developer</title>
-        <meta></meta>
       </Head>
-    </div>
+      <main className=''>
+        Home
+      </main>
+    </>
   )
 }
