4b8c51b940a4705569bf191104389b060b2cb08d
Gabriel Costa Ferreira <72426601+backcost@users.noreply.github.com>
2023-05-10 20:37:56 +0000

    colors

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
     extend: {
       fontFamily: {
         mont: ['var(--font-mont)', ...fontFamily.sans]
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", 
+        primaryDark: "#58E6D9",
       }
     },
   },
