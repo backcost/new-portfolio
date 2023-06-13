885232e5ee6412cff3c195dfc8c783b4134fff21
Gabriel Costa Ferreira <72426601+backcost@users.noreply.github.com>
2023-06-13 18:13:02 +0000

    logo fix

--- portfolio/src/components/Logo.js
+++ portfolio/src/components/Logo.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/legacy/image'
+import logo from '../../public/logo.png'
 
 const Logo = () => {
   return (
@@ -8,7 +9,7 @@ const Logo = () => {
         <Link href="/" className='w-16 h-16 rounded-full border border-solid 
         border-transparent dark:border-light'>
           <Image
-            src="../../public/logo.png"
+            src={logo}
             width={200}
             height={200}
             className='rounded-full'
