d9be9c0ac4407e0b79d000d4c79715b96a8b42f3
Gabriel Costa Ferreira <72426601+backcost@users.noreply.github.com>
2023-06-14 15:38:37 +0000

    image responsive

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -13,26 +13,23 @@ export default function Home() {
         <meta name='description' content="Gabriel Costa is a web developer from Brazil. Here you'll find some information about his carrer and some of his projects." />
       </Head>
       <TransitionEffect />
-      <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-        <Layout className='pt-0 md:pt-16 sm:pt-8'>
-          <div className='flex items-center justify-between w-full lg:flex-col'>
-            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
-              <AnimatedText text="Let's Build Something Great Together" className='!text-6xl 
-              !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl'/>
-              <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>I'm a passionate full stack developer dedicated to creating 
-                amazing and functional digital solutions. If you're looking for a 
-                customized website or web application that is visually appealing, 
-                intuitive, and efficient, you've come to the right place.</p>
-            </div>
-            <div className='w-1/2 md:w-full'> 
-              <Image src={profilePic} alt='Gabriel Costa' className='w-full h-auto lg:hidden 
-              md:inline-block md:w-full' priority
-              sizes='(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              50vw'/>
-            </div>
+      <main className='flex items-center justify-center mx-auto text-dark w-full max-w-[1400px] 
+      min-h-[78vh] dark:text-light'>
+        <div className='flex items-center justify-center w-full lg:flex-col lg:mx-2'>
+          <div className='w-1/2 flex flex-col items-center self-end lg:w-full lg:text-center lg:order-2 
+          lg:self-center'>
+            <AnimatedText text="Let's Build Something Great Together" className='!text-7xl 
+            !text-left xl:!text-5xl lg:!text-4xl lg:!text-center sm:!text-2xl'/>
+            <p className='mt-4 font-medium text-lg sm:text-xs'>I'm a passionate full stack developer dedicated to creating 
+              amazing and functional digital solutions. If you're looking for a 
+              customized website or web application that is visually appealing, 
+              intuitive, and efficient, you've come to the right place.</p>
           </div>
-        </Layout>
+          <div className='self-end lg:self-center sm:ml-10'> 
+            <Image src={profilePic} alt='Gabriel Costa' className='w-[420px] h-auto 
+            self-center lg:w-[280px]' priority/>
+          </div>
+        </div>
       </main>
     </>
   )
