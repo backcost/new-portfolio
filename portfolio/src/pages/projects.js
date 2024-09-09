da8307e486900e4fcd07f7d7e7fe3ce2d4c06586
backcost <gabriel_costa_ferreira@hotmail.com>
2024-09-09 15:06:56 +0200

    fixing error

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -57,43 +57,6 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
         </article>
     )
 }
-
-const Project = ({title, type, img, link, github}) => {
-    return(
-        <article className='w-full flex flex-col items-center justify-center rounded-2xl border
-        border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light 
-        xs:p-4'>
-            <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem]
-            bg-dark rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] 
-            xs:rounded-[1.5rem]' />
-            <Link href={link} target='_blank' className='w-full cursor-pointer
-            overflow-hidden rounded-lg'>
-                <FramerImage src={img} alt={title} className='w-full h-auto'
-                whileHover={{scale:1.05}}
-                transition={{duration:0.2}}
-                 />
-            </Link>
-
-            <div className='w-full flex flex-col items-start justify-between mt-4'>
-                <span className='text-primary font-medium text-xl 
-                dark:text-primaryDark lg:text-lg md:text-base'>{type}</span>
-                <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-                    <h2 className='my-2 w-full text-left text-3xl font-bold lg:text-2xl'>{title}</h2>
-                </Link>
-                <div className='w-full mt-2 flex items-center justify-between'>
-                    <Link href={link} target='_blank' className='text-lg font-semibold 
-                    underline md:text-base'>
-                        Visit
-                    </Link>
-                    <Link href={github} target='_blank' className='w-8 md:w-6'>
-                        <GithubIcon />
-                    </Link>
-                </div>
-            </div>
-        </article>
-    )
-}
-
 const projects = () => {
   return (
     <>
@@ -137,14 +100,6 @@ const projects = () => {
                         github="https://github.com/backcost/new-portfolio"
                         />
                     </div>
-                    {/*
-                    <div className='col-span-6 sm:col-span-12'>
-                        Project-1
-                    </div>
-                    <div className='col-span-6 sm:col-span-12'>
-                        Project-2
-                    </div>
-                    */}
                 </div>
             </Layout>
         </main>
